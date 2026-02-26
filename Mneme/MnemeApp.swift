afc4792af273c14f476f1c4ba24d441ec28c7c76
Idan Moshe - Home <moshei@theranica.com>
2026-02-26 14:12:15 +0200

    Initial Commit

---
+++ Mneme/MnemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MnemeApp.swift
+//  Mneme
+//
+//  Created by Idan Moshe on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MnemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
